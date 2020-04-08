3332fb815f625905b02b126c52a6e29d31efeb94
Andrey Afanasiev <andrey.afanasiev@gmail.com>
2020-04-08 14:54:15 +0200

    the function factorial(n) resolves all the numbers into a factorial and resutrn the value in concole by using the console.log function

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
-}
+    let num=prompt('Please, enter a number to resolve into a factorial');
+      if (num === 0 || num === 1)
+        return 1;
+      for (var i = num - 1; i >= 1; i--) {
+        num *= i;
+      }
+      return num;
+    }
+    
+
