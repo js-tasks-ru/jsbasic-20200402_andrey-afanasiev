a3520dcefd6027ba382eb8cd67b53921946381ab
Andrey Afanasiev <andrey.afanasiev@gmail.com>
2020-04-09 12:47:38 +0200

    the funtion factorial(n) calculates factorials of integers and returns 1 from 0, as requred.

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,13 +4,11 @@
  * @returns {number}
  */
 function factorial(n) {
-    let num=prompt('Please, enter a number to resolve into a factorial');
-      if (num === 0 || num === 1)
-        return 1;
-      for (var i = num - 1; i >= 1; i--) {
-        num *= i;
-      }
-      return num;
-    }
-    
+  if (n === 0 || n === 1)
+    return 1;
+  for (var i = n - 1; i >= 1; i--) {
+    n *= i;
+  }
+  return n;
+}
 
